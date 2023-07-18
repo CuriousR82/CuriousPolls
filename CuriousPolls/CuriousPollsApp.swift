cf93cf617088de50b2dcc596f994ebbfd563a187
CuriousR82 <rosajeon82@gmail.com>
2023-07-18 03:00:09 -0400

    Initial Commit

---
+++ CuriousPolls/CuriousPollsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuriousPollsApp.swift
+//  CuriousPolls
+//
+//  Created by Rosa Jeon on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct CuriousPollsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
